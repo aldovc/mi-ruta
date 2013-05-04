2d3e3b295b334f957c6a0a4f6f0c7d6994df0926
Aldo Vega (@aldovega) <aldo@ooyala.com>
2013-05-04 16:25:46 -0500

    Refactor folder structure

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,23 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Mi Ruta</title>
+
+  <!-- Included CSS Files, use foundation.css if you do not want minified code -->
+  <link rel="stylesheet" href="css/vendor/foundation/foundation.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+</body>
+</html>
