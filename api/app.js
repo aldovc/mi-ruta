55027b276b49fba34c7cb93842f02f92df209b7b
Aldo Vega (@aldovega) <aldo@ooyala.com>
2013-05-05 10:34:16 -0500

    Make use of postgre lib within node

--- api/app.js
+++ api/app.js
@@ -1,10 +1,20 @@
 var server = require('restify').createServer();
+var pg = require("pg");
+var conString = "pg://apidispatcher@localhost:5432/sample";
+var client = new pg.Client(conString);
+client.connect();
 
 
 function respond(req, res, next) {
-  console.log("req.params.origin:" + req.params.origin);
-  console.log("req.params.destintion:" + req.params.destination);
-  res.send('OK');
+  var query = client.query('SELECT NOW() AS "time"');
+  query.on('row', function(row, result) {
+    result.addRow(row);
+  });
+
+  query.on("end", function(result) {
+   console.log(result.rows); 
+   res.send(result.rows);
+  });
 }
 server.get('/routes/:origin/:destination', respond);
 server.listen(8080, function () {
