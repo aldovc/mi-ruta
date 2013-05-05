c1d3203d50c8fe625a1a8e3a59d97480c3447cb4
Aldo Vega (@aldovega) <aldo@ooyala.com>
2013-05-05 14:51:27 -0500

    Unlink muser model

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -11,7 +11,6 @@ class Home extends CI_Controller {
     $config = $CI->config->item('facebook');
     $this->load->library('facebook', $config);
     $this->load->library('session');
-    $this->load->model('muser');
   }
 
 
