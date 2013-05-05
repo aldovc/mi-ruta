9530c03ddbddd115a6f8f9117202bde56115f4e6
Aldo Vega (@aldovega) <aldo@ooyala.com>
2013-05-04 21:06:39 -0500

    Add test controller

---
+++ application/controllers/test.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+    echo "Test";
+	}
+}
+
+/* End of file home.php */
+/* Location: ./application/controllers/home.php */
